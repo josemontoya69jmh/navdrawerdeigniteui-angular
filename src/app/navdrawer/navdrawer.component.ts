36d242fd032553c901e1622e9ed36aec86be059d
josemontoya69jmh <2017josemontoya@gmail.com>
2020-02-06 19:17:29 +0100

    3 files modified

--- src/app/navdrawer/navdrawer.component.ts
+++ src/app/navdrawer/navdrawer.component.ts
@@ -7,10 +7,10 @@ import { IgxNavigationDrawerComponent } from "igniteui-angular";
   styleUrls: ["./navdrawer.component.scss"],
   templateUrl: "./navdrawer.component.html"
 })
-export class NavdrawerComponent  {
-// Comprobar esta variable
+export class NavdrawerComponent {
+  // Comprobar esta variable
   public navItems = [
-    { name: "favorite", text: "Avatar" },
+    { name: "home", text: "Inico" },
     { name: "list", text: "Badge" },
     { name: "group_work", text: "Button Group" },
     { name: "home", text: "Card" },
@@ -18,46 +18,34 @@ export class NavdrawerComponent  {
     { name: "date_range", text: "Date picker" },
     { name: "all_out", text: "Dialog" },
     { name: "web", text: "Forms" }
-   // { name: "delete", text: "Borrar" },
-  //  { name: "list", text: "List" },
-   // { name: "arrow_back", text: "Navbar" },
+    // { name: "delete", text: "Borrar" },
+    //  { name: "list", text: "List" },
+    // { name: "arrow_back", text: "Navbar" },
     //{ name: "menu", text: "Navdrawer" },
     //{ name: "poll", text: "Progress Indicators" },
-   // { name: "linear_scale", text: "Slider" },
-   // { name: "swap_vert", text: "Scroll" },
-   // { name: "feedback", text: "Snackbar" },
-   // { name: "tab", text: "Tabbar" },
+    // { name: "linear_scale", text: "Slider" },
+    // { name: "swap_vert", text: "Scroll" },
+    // { name: "feedback", text: "Snackbar" },
+    // { name: "tab", text: "Tabbar" },
     //{ name: "android", text: "Toast" }
   ];
-  public selected = "Avatar";
+
+  public selected = "";
 
   @ViewChild(IgxNavigationDrawerComponent, { static: true })
-  // La instancia drawer nos devuelve todo lo que podemos hacer con 
+  // La instancia drawer nos devuelve todo lo que podemos hacer con
   // position
   public drawer: IgxNavigationDrawerComponent;
-
-
-  public drawerState = {
-// Con la miniTemplate controlamos la miniatura de los iconos
-    miniTemplate: false,
-    // aqui controlamos junto con [isOpen]en el html si aparece abierto o cerrado al iicio
-    open: false,
-    // 
-    pin: false
-  };
 
   /** Funcion espupenda para ver que nos devuelve la instancia drawer y que podemos
    hacer con ella
    */
   public navigate(item) {
-  console.log('reciboelitem',item)
+    
     this.selected = item.text;
-this.drawer.position= 'right';
+    //  this.drawer.position= 'right';
 
- this.drawer.close();
- console.log('drawer instancia', this.drawer.isOpen);
-    // if (!this.drawer.pin) {
-    //   this.drawer.close();
-    // }
+    this.drawer.close();
+    // console.log("drawer instancia", this.drawer.isOpen);
   }
 }
